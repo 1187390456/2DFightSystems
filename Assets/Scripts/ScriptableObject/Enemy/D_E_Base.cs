04e94f4edf91db793cc740d01d6a94b3c2aba86a
XuChenMing <1187390456@qq.com>
2022-09-23 19:03:25 +0800

    修复警备状态过度

--- Assets/Scripts/ScriptableObject/Enemy/D_E_Base.cs
+++ Assets/Scripts/ScriptableObject/Enemy/D_E_Base.cs
@@ -9,7 +9,7 @@ public class D_E_Base : ScriptableObject
     [Header("边缘检测距离")] public float edgeCheckDistance = 1.0f;
     [Header("最小警备距离")] public float minDetectedDistance = 6.5f;
     [Header("最大警备距离")] public float maxDetectedDistance = 9.5f;
-    [Header("可近战攻击的距离")] public float canMeleeDistance = 1.5f;
-    [Header("击晕次数")] public int stunCount = 3;
+    [Header("可近战攻击的距离")] public float canMeleeDistance = 2.0f;
+    [Header("击晕次数")] public int stunCount = 5;
     [Header("击退速度")] public Vector2 knockbackSpeed = new Vector2(5.0f, 10.0f);
 }
--- Assets/Scripts/ScriptableObject/Enemy/D_E_MeleeAttack.cs
+++ Assets/Scripts/ScriptableObject/Enemy/D_E_MeleeAttack.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MeleeAttack", menuName = "Data/Enemy Data/State Data/MeleeAttack Data")]
 public class D_E_MeleeAttack : ScriptableObject
 {
-    [Header("近战攻击半径")] public float meleeAttackRadius = 0.65f;
+    [Header("近战攻击半径")] public float meleeAttackRadius = 1.0f;
     [Header("近战攻击伤害")] public float meleeAttackDamage = 10.0f;
 }
