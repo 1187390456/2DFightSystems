80ea9152e9de2df08bb8fad79b4d166cfb6f1067
XuChenMing <1187390456@qq.com>
2022-09-30 19:06:46 +0800

    修改无限复活怪物

--- Assets/Scripts/Enemy/BaseState/E_Dead.cs
+++ Assets/Scripts/Enemy/BaseState/E_Dead.cs
@@ -45,7 +45,8 @@ public class E_Dead : E_State
         currentTransparent = 1;
         entity.isDead = false;
         entity.currentHealth = entity.entityData.maxHealth;
-        entity.aliveGobj.transform.position = GameManager.Instance.GetRandPos();
+        // entity.aliveGobj.transform.position = GameManager.Instance.GetRandPos();
+        entity.aliveGobj.transform.position = new Vector2(entity.aliveGobj.transform.position.x, entity.aliveGobj.transform.position.y + 1.0f);
         entity.aliveGobj.SetActive(true);
         if (deadData.isMonster)
         {
