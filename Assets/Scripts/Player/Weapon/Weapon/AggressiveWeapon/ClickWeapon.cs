5208ba30eed489b39a6c7a41850b0c1e27413f11
XuChenMing <1187390456@qq.com>
2022-10-25 19:44:25 +0800

    拳头特效

--- Assets/Scripts/Player/Weapon/Weapon/AggressiveWeapon/ClickWeapon.cs
+++ Assets/Scripts/Player/Weapon/Weapon/AggressiveWeapon/ClickWeapon.cs
@@ -9,8 +9,21 @@ public class ClickWeapon : AggressiveWeapon
         base.AnimationStart();
         WeaponAttackInfo weaponAttackInfo = aggressionWeaponData.attackInfos[attackIndex];
         var randomIndex = Random.Range(0, weaponAttackInfo.effectArr.Length);
-        var gobj = ParticleManager.instance.CreateParticle(weaponAttackInfo.effectArr[randomIndex], transform, Quaternion.Euler(0.0f, 0.0f, weaponAttackInfo.rot));
-        gobj.transform.localScale = new Vector3(Player.Instance.movement.facingDireciton, -Player.Instance.movement.facingDireciton, 1);
+        var rot = Quaternion.Euler(0.0f, 0.0f, weaponAttackInfo.rot);
+        Debug.Log(weaponAttackInfo.effectArr[randomIndex].name);
+        if (weaponAttackInfo.attackName == "Fist")
+        {
+            rot = Quaternion.Euler(0.0f, -90.0f, 90.0f);
+        }
+        var gobj = ParticleManager.instance.CreateParticle(weaponAttackInfo.effectArr[randomIndex], transform, rot);
+        if (weaponAttackInfo.attackName == "Fist")
+        {
+            gobj.transform.localScale = new Vector3(3.0f, 3.0f, 3.0f * -Player.Instance.movement.facingDireciton);
+        }
+        else
+        {
+            gobj.transform.localScale = new Vector3(Player.Instance.movement.facingDireciton, -Player.Instance.movement.facingDireciton, 1);
+        }
     }
 
     public override void Enter()
